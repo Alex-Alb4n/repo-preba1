06bd6dff2d56760f34ca9f72e0404a8ad9bc8a4d
Alex Albán <cedealexcito@gmail.com>
2026-03-01 17:09:19 -0500

    Add files via upload

---
+++ archivone.py
@@ -0,0 +1,7 @@
+name = "ALEX VINICIO"
+apellido = "ALBÁN CEDEÑO"
+saludo =f"Mi nombre es {name} y mi apellido es {apellido}"
+print(saludo + "estoy aprendiendo a programar en Python")
+print("esto es una prueba de practica de git como ignorar archivos txr")
+print("esto en una prueba rapida de git ignore")
+
