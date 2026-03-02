dc9bfd84bbb54363c18ece50a2279fa2e9f05bb8
Alex Albán <cedealexcito@gmail.com>
2026-03-02 10:59:16 -0500

    subiendo cambios a github #1

--- archivone.py
+++ archivone.py
@@ -2,6 +2,14 @@ name = "ALEX VINICIO"
 apellido = "ALBÁN CEDEÑO"
 saludo =f"Mi nombre es {name} y mi apellido es {apellido}"
 print(saludo + "estoy aprendiendo a programar en Python")
-print("esto es una prueba de practica de git como ignorar archivos txr")
+print("esto es una prueba de practica de git como ignorar archivos txt")
 print("esto en una prueba rapida de git ignore")
+# realizando una pequeña operación matematica para practicar en git y github
+def permiso(edad):
+    if edad >= 18:
+        return "Eres un adulto, por lo que si vas a poder conducir"
+    else :
+        return "Eres aun menor de edad, por lo que aun no vas a poder conducir"
 
+
+print(permiso(18))
