4aaf3803f7725d1513d8f5ba89447ff0ec2af9ad
Alex Albán <cedealexcito@gmail.com>
2026-03-03 17:00:41 -0500

    usando git fetch archivone.py

--- archivone.py
+++ archivone.py
@@ -16,4 +16,6 @@ print(permiso(15))
 print("Que tengas un exelente dia y recuerda practicar mucho para lograr tus objetivos")
 print( "mostrando el six sevent de mikel arteta")
 print("este es mi año por que haci me lo creo yo")
+#esto lo vere usando el comando git fetch
+print("git hello good bey")
 
